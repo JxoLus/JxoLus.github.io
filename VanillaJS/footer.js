5e2a38ca4b05709c614e8d73856b5a9cb3841ae0
xoLus <xoLus@naver.com>
2023-01-06 15:58:26 +0900

    add fortune tab and weather tab on footer-ui

--- VanillaJS/footer.js
+++ VanillaJS/footer.js
@@ -1,14 +1,20 @@
 const windows = document.querySelector(".footer-ui__windows");
 const clock = document.querySelector(".footer-ui__clock");
 const todo = document.querySelector(".footer-ui__todo");
+const fortune = document.querySelector(".footer-ui__fortune");
+const weather = document.querySelector(".footer-ui__weather");
 
 const windowsTab = document.querySelector(".main-tabs__windows");
 const clockTab = document.querySelector(".main-tabs__clock");
 const todoTab = document.querySelector(".main-tabs__todo");
+const fortuneTab = document.querySelector(".main-tabs__fortune");
+const weatherTab = document.querySelector(".main-tabs__weather");
 
 let windowsIsOpened = false;
 let clockIsOpened = false;
 let todoIsOpened = false;
+let fortuneIsOpened = false;
+let weatherIsOpened = false;
 
 function usedNow() {}
 
@@ -45,11 +51,33 @@ function showTab(who) {
     }
   }
 
+  if (who == "fortune") {
+    if (fortuneIsOpened == false) {
+      fortuneIsOpened = true;
+      fortuneTab.classList.remove("hidden");
+    } else if (fortuneIsOpened == true) {
+      fortuneIsOpened = false;
+      fortuneTab.classList.add("hidden");
+    }
+  }
+
+  if (who == "weather") {
+    if (weatherIsOpened == false) {
+      weatherIsOpened = true;
+      weatherTab.classList.remove("hidden");
+    } else if (weatherIsOpened == true) {
+      weatherIsOpened = false;
+      weatherTab.classList.add("hidden");
+    }
+  }
+
   console.log(
-    `windowsIsOpened: ${windowsIsOpened}\nclockIsOpened: ${clockIsOpened}\ntodoIsOpened: ${todoIsOpened}`
+    `windowsIsOpened: ${windowsIsOpened}\nclockIsOpened: ${clockIsOpened}\ntodoIsOpened: ${todoIsOpened}\nfortuneIsOpened: ${fortuneIsOpened}\nweatherIsOpened: ${weatherIsOpened}`
   );
 }
 
 windows.addEventListener("click", () => showTab("windows"));
 clock.addEventListener("click", () => showTab("clock"));
 todo.addEventListener("click", () => showTab("todo"));
+fortune.addEventListener("click", () => showTab("fortune"));
+weather.addEventListener("click", () => showTab("weather"));
