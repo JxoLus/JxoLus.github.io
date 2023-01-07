67ae80c6b2a1b45834b746d0f8af2f36bdaa5a31
xoLus <xoLus@naver.com>
2023-01-07 15:40:31 +0900

    중간 저장

--- VanillaJS/js/background.js
+++ VanillaJS/js/background.js
@@ -1,37 +1,34 @@
 let currentBackground = 100;
-
+backgroundChange();
 function backgroundChange() {
   const BODY = document.body;
 
-  if (currentBackground === 1) {
-    BODY.classList.remove("chosenOne");
-  } else if (currentBackground === 2) {
-    BODY.classList.remove("chosenTwo");
-  } else if (currentBackground === 3) {
-    BODY.classList.remove("chosenThree");
-  }
-
   const random = Math.floor(Math.random() * 3);
-
-  console.log(random + 1, currentBackground);
-  if (currentBackground != 100 && random + 1 == currentBackground) {
-    backgroundChange();
+  
+  if (currentBackground != 100 && random === currentBackground) {
+    return backgroundChange();
+  }
+  
+  if (currentBackground === 0) {
+    BODY.classList.remove("chosenOne");
+  } else if (currentBackground === 1) {
+    BODY.classList.remove("chosenTwo");
+  } else if (currentBackground === 2) {
+    BODY.classList.remove("chosenThree");
   }
 
   if (random === 0) {
     BODY.classList.add("chosenOne");
-    currentBackground = 1;
+    currentBackground = 0;
   } else if (random === 1) {
     BODY.classList.add("chosenTwo");
-    currentBackground = 2;
+    currentBackground = 1;
   } else {
     BODY.classList.add("chosenThree");
-    currentBackground = 3;
+    currentBackground = 2;
   }
-
-  console.log(currentBackground);
 }
 
-backgroundChange();
+
 
 document.querySelector(".fa-image").addEventListener("click", backgroundChange);
---
+++ VanillaJS/js/weather.js
@@ -0,0 +1,21 @@
+let geoGet = false
+
+function onGeoSuccess(position) {
+  geoGet = true;
+
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const API_KEY = "6da854d8c7c347ea2aa39596d111449d";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main
+    });
+}
+function onGeoError() {
+  geoGet = false
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
