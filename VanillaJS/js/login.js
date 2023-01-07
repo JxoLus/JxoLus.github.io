c7fb4ab4ddd56e28b273b26185540578271b6e4d
xoLus <xoLus@naver.com>
2023-01-07 19:44:57 +0900

    feat: weather

--- VanillaJS/js/login.js
+++ VanillaJS/js/login.js
@@ -3,6 +3,9 @@ const passwordInput = document.querySelector("#password");
 const pressAnyKey = document.querySelector(".press-any-key");
 const passwordWrong = document.querySelector(".password-wrong");
 
+const welcome = document.querySelector(".welcome");
+const welcomeUser = document.querySelector(".welcome-user");
+
 let username = usernameInput.value;
 let password = passwordInput.value;
 let pakIsShowed = false;
@@ -25,31 +28,31 @@ function getPassword() {
       }
     }
   } else {
-    if (pwIsShowed === true) {
-      if (localStorage.getItem("password") === password) {
-        console.log("enter");
+    if (localStorage.getItem("password") === password) {
+      pressAnyKey.classList.remove("hidden");
+      pakIsShowed = true;
+      if (pwIsShowed === true) {
         passwordWrong.classList.add("hidden");
-        pressAnyKey.classList.remove("hidden");
-        pakIsShowed = true;
         pwIsShowed = false;
       }
+    } else {
+      pressAnyKey.classList.add("hidden");
+      pakIsShowed = false;
     }
   }
 }
 
 function goToMainPage() {
-  if (pakIsShowed === true) {
-    if (uniIsShowed === true) {
-      localStorage.setItem("username", username);
-      localStorage.setItem("password", password);
+  if (uniIsShowed === true) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    location.href = "main.html";
+  } else {
+    if (localStorage.getItem("password") === password) {
       location.href = "main.html";
     } else {
-      if (localStorage.getItem("password") === password) {
-        location.href = "main.html";
-      } else {
-        passwordWrong.classList.remove("hidden");
-        pwIsShowed = true;
-      }
+      passwordWrong.classList.remove("hidden");
+      pwIsShowed = true;
     }
   }
 }
@@ -58,13 +61,22 @@ function login() {
   if (localStorage.getItem("username") === null) {
     uniIsShowed = true;
   } else {
-    document.querySelector(".welcome-user").innerText = localStorage.getItem("username");
+    document.querySelector(".welcome-user").innerText =
+      localStorage.getItem("username");
     usernameInput.classList.add("hidden");
-    document.querySelector(".welcome").style.animation = "welcomeAnimation-welcome 1s ease-in-out;";
+    welcome.style.animation = "welcomeAnimation-welcome 1s ease-in-out";
+    welcome.style.animationFillMode = "forwards";
+    welcomeUser.classList.remove("hidden");
+    welcomeUser.style.animation = "welcomeAnimation-user 1s ease-in-out";
+    welcomeUser.style.animationFillMode = "forwards";
     uniIsShowed = false;
   }
 }
 
 login();
 
-document.body.addEventListener("click", goToMainPage);
+document.body.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    goToMainPage();
+  }
+});
--- VanillaJS/js/weather.js
+++ VanillaJS/js/weather.js
@@ -1,23 +1,23 @@
-let geoGet = false
+const weatherNow = document.querySelector(".main-tabs__weather-weather");
+const API_KEY = "6da854d8c7c347ea2aa39596d111449d";
+
+let geoGet = false;
 
 function onGeoSuccess(position) {
   geoGet = true;
 
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const API_KEY = "6da854d8c7c347ea2aa39596d111449d";
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const city = document.querySelector(".main-tabs-weather-city");
-      const weather = document.querySelector(".main-tabs-weather-weather");
-      city.innerText = data.name;
-      weather.innerText = data.weather[0].main;
+      weatherNow.innerText = `${data.name} / ${data.main.temp}°C`;
     });
 }
 function onGeoError() {
-  geoGet = false
+  geoGet = false;
+  alert("location fail");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
